778cfbb43ea936af9b8afdc38da5b5497c4ddd41
Taz Hinkle <hinkle.taz@gmail.com>
2022-04-19 21:38:13 -0600

    Add twilio and .env variables

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const twilioPhone = process.env.TWILIO_PHONE_OUT
 const Koa = require('koa')
 
 const app = new Koa()
+
+
 
 app.listen(3000, () => {
     console.log('server started on port 3000')
