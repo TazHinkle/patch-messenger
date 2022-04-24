289809565f581cbf7c9235aed015c9ef9588179e
Taz Hinkle <hinkle.taz@gmail.com>
2022-04-24 17:59:21 -0600

    Update deploy process for expiring ecr token

--- deploy_image_to_aws.sh
+++ deploy_image_to_aws.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/h9v1l3n3
+
 docker tag patch-messenger-repository-public:latest public.ecr.aws/h9v1l3n3/patch-messenger-repository-public:latest
 
 docker push public.ecr.aws/h9v1l3n3/patch-messenger-repository-public:latest
